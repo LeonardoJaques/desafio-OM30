1b8791775046b6f6b563032dac58ac208ba4651e
Leonardo Jaques <jaques.projetos@outlook.com>
2023-04-07 14:44:52 -0300

    add: settings for tailwindcss and postcss in vuejs

---
+++ desafio-front-end-vue-js/src/components/HomePage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="py-4 flex justify-center">
+   HomePage
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class HomePage extends Vue {
+}
+</script>
+
+
